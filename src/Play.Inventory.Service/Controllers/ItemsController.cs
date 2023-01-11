1840eff4069f39a289b01ed61090524c9a127b4d
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-01-11 20:04:35 +0300

    add 2 endpoints for this services

--- src/Play.Inventory.Service/Controllers/ItemsController.cs
+++ src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -24,11 +24,36 @@ namespace Play.Inventory.Service.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
         {
-            if(userId == Guid.Empty) return BadRequest();
+            if (userId == Guid.Empty) return BadRequest();
 
             var items = (await itemsRepository.GetAllAsync(item => item.UserId == userId)).Select(item => item.AsDto());
 
             return Ok(items);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(GrandItemsDto grandItemsDto)
+        {
+            var inventoryItem = await itemsRepository.GetAsync(item => item.UserId == grandItemsDto.UserId && item.CatalogItemId == grandItemsDto.CatalogItemId);
+            if (inventoryItem is null)
+            {
+                inventoryItem = new InventoryItem
+                {
+                    CatalogItemId = grandItemsDto.CatalogItemId,
+                    UserId = grandItemsDto.UserId,
+                    Quantity = grandItemsDto.Quantity,
+                    AcquiredDate = DateTimeOffset.UtcNow
+                };
+
+                await itemsRepository.CreateAsync(inventoryItem);
+            }
+            else
+            {
+                inventoryItem.Quantity += grandItemsDto.Quantity;
+                await itemsRepository.UpdateAsync(inventoryItem);
+            }
+
+            return Ok();
         }
     }
 }
